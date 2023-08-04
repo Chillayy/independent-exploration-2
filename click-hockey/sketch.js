cf2d9c3728a35968472203284b5ca597fa4cdf3f
Chillayy <shemar.pradia@gmail.com>
2023-08-04 12:37:43 -0500

    final commit

--- click-hockey/sketch.js
+++ click-hockey/sketch.js
@@ -5,57 +5,57 @@ let ySpeed = 2;
 let score = 0;
 
 function setup() {
-    createCanvas(500, 500);
+  createCanvas(500, 500);
 
-    noStroke();
+  noStroke();
 
-    rectMode(CENTER);
+  rectMode(CENTER);
 }
 
 function draw() {
-    background(222);
+  background(222);
 
-    // draw goal
-    fill(0, 255 0);
-    rect(250, 490, 60, 20);
+  // draw goal
+  fill(0, 255, 0);
+  rect(250, 490, 60, 20);
 
-    // draw ball
-    fill(255, 0, 255);
-    rect(30, xPos, yPos);
+  // draw ball
+  fill(255, 0, 255);
+  rect(xPos, yPos, 30, 30);
 
-    // draw score
-    fill(18);
-    textSize(20);
-    text("Score: " + score, 0, 0)
+  // draw score
+  fill(18);
+  textSize(20);
+  text("Score: " + score, 20, 20);
 
-    // check if in goal
-    if (xPos + 15 <= 220 && xPos - 15 <= 280 && yPos + 15 >= 480) {
-        score;
-        // reset position and speed
-        xPos = random(15, 485);
-        yPos = 0;
-        xSpeed = random(-5, 5);
-        ySpeed = random(0, 10);
-    }
+  // check if in goal
+  if (xPos >= 220 && xPos <= 280 && yPos >= 480) {
+    score++;
+    // reset position and speed
+    xPos = random(15, 485);
+    yPos = 0;
+    xSpeed = random(-5, 5);
+    ySpeed = random(0, 10);
+  }
 
-    // move forward
-    xPos += xSpeed;
-    yPos += ySpeed;
+  // move forward
+  xPos += xSpeed;
+  yPos += ySpeed;
 
-    // wrap around if boundary exceeded
-    if (xPos < 0) {
-        xPos = 500;
-    }
-    if (xPos > 500) {
-        xPos = 0;
-    }
-    if (yPos > 500) {
-        yPos = 0;
-        score--;
-    }
+  // wrap around if boundary exceeded
+  if (xPos < 0) {
+    xPos = 500;
+  }
+  if (xPos > 500) {
+    xPos = 0;
+  }
+  if (yPos > 500) {
+    yPos = 0;
+    score--;
+  }
 }
 
 function mouseClicked() {
-    xSpeed = random(-5, 5);
-    ySpeed = random(0, 5);
+  xSpeed = random(-5, 5);
+  ySpeed = random(0, 5);
 }
--- intervals/script.js
+++ intervals/script.js
@@ -3,3 +3,11 @@ let time = 0;
 // reference to the <span> containing the number
 let counterElement = document.getElementById("time");
 
+// Update the time and the counterElement every second
+const updateCounter = () => {
+  time++;
+  counterElement.textContent = time;
+};
+
+// Call the updateCounter function every 1000 milliseconds (1 second)
+const intervalID = setInterval(updateCounter, 1000);
